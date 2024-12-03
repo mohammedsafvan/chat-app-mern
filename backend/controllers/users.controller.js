3807d7004227dbaa95ef008af0f30c55a74331dc
Mohammed Safvan <mohammedsafvannk@gmail.com>
2024-12-04 04:38:30 +0530

    some edit

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -6,6 +6,7 @@ export const getUserForSideBar = async (req, res) => {
     const allUsers = await UserModel.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
+    res.status(200).json(allUsers);
   } catch (e) {
     console.error("Error in getting Users.", e.message);
     res.status(400).json({ error: "Internal server error" });
