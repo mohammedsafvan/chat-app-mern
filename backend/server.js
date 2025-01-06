5d8106868d4b6b1a9925832507929dffe32d1d9b
Mohammed Safvan <mohammedsafvannk@gmail.com>
2025-01-06 17:05:34 +0530

    deploy ready

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,24 @@ import messageRouter from "./routes/messages.routes.js";
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import usersRoute from "./routes/users.routes.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", usersRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDb();
